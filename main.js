332ac85c31f3d156662bcd6cc72ae408760a1378
DP <deepakpattnayak2013@gmail.com>
2021-03-06 12:24:03 +0530

    js file changed

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-// function darkMode(){
-//     element = document.body;
-//     element.classList.toggle("darkMode");
-// }
-
-// function toggleTheme(){
-//     var lt = document.getElementById("lightCSS"),
-//     dt = document.getElementById("darkkCSS");
-
-//     if(lt.disabled == "disabled"){
-//         lt.disabled = undefined;
-//         dt.disabled = "disabled";
-//     }
-//     else{
-//         lt.disabled = "disabled";
-//         dt.disabled = undefined;
-//     }
-// }
-
 function toggle(){
     var a = document.getElementById("theme");
     a.x = 'darkTheme' == a.x ? 'lightTheme' : 'darkTheme';
