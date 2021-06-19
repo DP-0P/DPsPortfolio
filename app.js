278f802a6a256dc5cb69d877921691f6ffb45bed
DP_OP <deepakpattnayak2013@gmail.com>
2021-06-19 11:55:31 +0530

    scroll bar done

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ particlesJS("particles-js", {
     },
     "move": {
       "enable": true,
-      "speed": 500,
+      "speed": 2.5,
       "direction": "none",
       "random": true,
       "straight": false,
--- scroll.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    if (!$.browser.webkit) {
-        $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
-    }
-});
